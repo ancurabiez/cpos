60c89b43d2035c177907aef840f55dee1f99a369
Lulus Wijayakto <l.wijayakto@yahoo.com>
2014-06-07 20:02:16 +0700

    First Time

---
+++ build_benchmark.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include "cpos.h"
+
+
+int main(void)
+{
+  int i;
+  int count = 10000;
+  clock_t start, end;
+  
+  start = clock();
+  
+  struct isofield_cfg *ifield;
+  struct isomsg *imsg;
+  char buffer[1024];
+
+  ifield = cpos_init("cpos.cfg");
+  if (!ifield)
+    return 1;
+    
+  for (i = 0; i < count; i++) {
+    imsg = cpos_msg_new();
+    if (!imsg)
+      return 1;
+   
+    cpos_build_set_field(imsg, 2, "4729700101137365", 16);
+    cpos_build_set_field(imsg, 3, "880891", 6);
+    cpos_build_set_field(imsg, 4, "000000000000", 12);
+    cpos_build_set_field(imsg, 7, "1127041312", 10);
+    cpos_build_set_field(imsg, 11, "036030", 6);
+    cpos_build_set_field(imsg, 12, "111312", 6);
+    cpos_build_set_field(imsg, 13, "1127", 4);
+    cpos_build_set_field(imsg, 14, "0000", 4);
+    cpos_build_set_field(imsg, 15, "1128", 4);
+    cpos_build_set_field(imsg, 18, "6971", 4);
+    cpos_build_set_field(imsg, 32, "717", 3);
+    cpos_build_set_field(imsg, 33, "226", 3);
+    cpos_build_set_field(imsg, 37, "013331036030", 12);
+    cpos_build_set_field(imsg, 41, "00000036", 8);
+    cpos_build_set_field(imsg, 42, "000            ", 15);
+    cpos_build_set_field(imsg, 43, "ABCDFG ABCDEFGH                  1234567", 40);
+    cpos_build_set_field(imsg, 49, "450", 3);
+    cpos_build_set_field(imsg, 61, "1339617306680", 13);
+    cpos_build_set_field(imsg, 103, "054201", 6);
+
+    /* build without length header */
+    cpos_build_msg(ifield, imsg, "0210", buffer, sizeof(buffer), 0);
+    //printf("Build without len: [%s]\n", buffer);
+
+    /* build with length header */
+    //cpos_build_msg(ifield, imsg, "0810", buffer, sizeof(buffer), 1);
+    //printf("Build with len: [%s]\n", buffer);
+
+    cpos_msg_free(imsg);
+  }
+  
+  cpos_close(ifield);
+  
+  end = clock();
+  
+  printf("Count: %d\n", count);
+  printf("Elapsed: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+  
+  return 0;
+}
---
+++ unpack_benchmark.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include "cpos.h"
+
+int main(void)
+{
+  int i;
+  int count = 10000;
+  char iso8583[512];
+  clock_t start, end;
+  
+  strcpy(iso8583, "0210F23E400188E0800800000000020000001647297"
+		  "0010113736588089100000000000011270413120360301113121"
+		  "127000011286971037170322601333103603000000036"
+		  "000            ABCDFG ABCDEFGH                  1234567"
+		  "360013133961730668006054201");
+  
+  start = clock();
+  
+  struct isofield_cfg *ifield;
+  struct isomsg *imsg;
+
+  ifield = cpos_init("cpos.cfg");
+  if (!ifield)
+    return 1;
+    
+  for (i = 0; i < count; i++) {
+    imsg = cpos_msg_new();
+    if (!imsg)
+      return 1;
+    
+    if (cpos_parse(ifield, imsg, (uint8_t*) iso8583))
+      return 1;
+    
+    //for (i = 0; i < ISO_BIT_LEN; i++) {
+     // if (imsg[i].data)
+     //   printf("BIT [%d] [%d] [%s]\n", i, imsg[i].len, (char*)imsg[i].data);
+    //}
+
+    cpos_msg_free(imsg);
+  }
+  
+  cpos_close(ifield);
+  
+  end = clock();
+  
+  printf("Count: %d\n", count);
+  printf("Elapsed: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+  
+  return 0;
+}
